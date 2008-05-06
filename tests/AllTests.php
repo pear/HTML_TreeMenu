46c311b0612ee74bdb3c19c108ad9b129cc60c14
Chuck Burgess <ashnazg@php.net>
2008-05-06 14:05:09 +0000

    add phpunit test runner
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_TreeMenu/trunk@259156 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ tests/AllTests.php
@@ -0,0 +1,131 @@
+<?php
+
+/**
+ * Master Unit Test Suite file for HTML_TreeMenu
+ * 
+ * This top-level test suite file organizes 
+ * all class test suite files, 
+ * so that the full suite can be run 
+ * by PhpUnit or via "pear run-tests -u". 
+ *
+ * PHP versions 4 and 5
+ *
+ * @category   HTML
+ * @package    HTML_TreeMenu
+ * @subpackage UnitTesting
+ * @author     Chuck Burgess <ashnazg@php.net>
+ * @license    http://www.opensource.org/licenses/bsd-license.php BSD License
+ * @version    CVS: $Id$
+ * @link       http://pear.php.net/package/HTML_TreeMenu
+ * @since      1.2.2
+ */
+
+
+/**
+ * Check PHP version... PhpUnit v3+ requires at least PHP v5.1.4
+ */
+if (version_compare(PHP_VERSION, "5.1.4") < 0) {
+    // Cannnot run test suites
+    echo 'Cannot run test suite via PhpUnit... requires at least PHP v5.1.4.' . PHP_EOL;
+    echo 'Use "pear run-tests -p html_treemenu" to run the PHPT tests directly.' . PHP_EOL;
+    exit(1);
+}
+
+
+/**
+ * Derive the "main" method name
+ * @internal PhpUnit would have to rename PHPUnit_MAIN_METHOD to PHPUNIT_MAIN_METHOD
+ *           to make this usage meet the PEAR CS... we cannot rename it here.
+ */
+if (!defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'HTML_TreeMenu_AllTests::main');
+}
+
+
+/*
+ * Files needed by PhpUnit
+ */
+require_once 'PHPUnit/Framework.php';
+require_once 'PHPUnit/TextUI/TestRunner.php';
+require_once 'PHPUnit/Extensions/PhptTestSuite.php';
+
+/*
+ * You must add each additional class-level test suite file here
+ */
+// there are no PhpUnit test files... only PHPTs.. so nothing is listed here
+
+/**
+ * directory where PHPT tests are located
+ */
+define('HTML_TREEMENU_DIR_PHPT', dirname(__FILE__));
+
+/**
+ * Master Unit Test Suite class for HTML_TreeMenu
+ * 
+ * This top-level test suite class organizes 
+ * all class test suite files, 
+ * so that the full suite can be run 
+ * by PhpUnit or via "pear run-tests -up html_treemenu". 
+ *
+ * @category   HTML
+ * @package    HTML_TreeMenu
+ * @subpackage UnitTesting
+ * @author     Chuck Burgess <ashnazg@php.net>
+ * @license    http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @version    Release: @package_version@
+ * @link       http://pear.php.net/package/XML_Util
+ * @since      1.2.2
+ */
+class HTML_TreeMenu_AllTests
+{
+
+    /**
+     * Launches the TextUI test runner
+     *
+     * @return void
+     * @uses PHPUnit_TextUI_TestRunner
+     */
+    public static function main()
+    {
+        PHPUnit_TextUI_TestRunner::run(self::suite());
+    }
+
+
+    /**
+     * Adds all class test suites into the master suite
+     *
+     * @return PHPUnit_Framework_TestSuite a master test suite
+     *                                     containing all class test suites
+     * @uses PHPUnit_Framework_TestSuite
+     */ 
+    public static function suite()
+    {
+        $suite = new PHPUnit_Framework_TestSuite(
+            'HTML_TreeMenu Full Suite of Unit Tests');
+
+        /*
+         * You must add each additional class-level test suite name here
+         */
+        // there are no PhpUnit test files... only PHPTs.. so nothing is listed here
+
+        /*
+         * add PHPT tests
+         */
+        $phpt = new PHPUnit_Extensions_PhptTestSuite(HTML_TREEMENU_DIR_PHPT);
+        $suite->addTestSuite($phpt);
+
+        return $suite;
+    }
+}
+
+/**
+ * Call the main method if this file is executed directly
+ * @internal PhpUnit would have to rename PHPUnit_MAIN_METHOD to PHPUNIT_MAIN_METHOD
+ *           to make this usage meet the PEAR CS... we cannot rename it here.
+ */
+if (PHPUnit_MAIN_METHOD == 'HTML_TreeMenu_AllTests::main') {
+    HTML_TreeMenu_AllTests::main();
+}
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+?>
