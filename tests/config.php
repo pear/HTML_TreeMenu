b8b9696677e0578e6a42fa9a13c4c4911fda5d3d
Daniel Convissor <danielc@php.net>
2011-12-10 02:20:23 +0000

    A nicer way to set the test DSN from environment variables.
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_TreeMenu/trunk@320824 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/config.php
+++ tests/config.php
@@ -1,17 +1,21 @@
 <?php
 
-if (!empty($_ENV['MYSQL_TEST_USER'])) {
-    $dbDsn = 'mysqli://' . rawurlencode($_ENV['MYSQL_TEST_USER'])
-        . ':' . rawurlencode($_ENV['MYSQL_TEST_PASSWD']);
-    if (empty($_ENV['MYSQL_TEST_SOCKET'])) {
-        $dbDsn .= '@' . $_ENV['MYSQL_TEST_HOST'];
-        if (!empty($_ENV['MYSQL_TEST_PORT'])) {
-            $dbDsn .= ':' . $_ENV['MYSQL_TEST_PORT'];
-        }
-    } else {
-        $dbDsn .= '@unix(' . $_ENV['MYSQL_TEST_SOCKET'] . ')';
-    }
-    $dbDsn .= '/' . rawurlencode($_ENV['MYSQL_TEST_DB']);
+if (empty($_ENV['MYSQL_TEST_USER'])) {
+    $dbDsn = 'mysqli://USER:PSWD@HOST/DBNAME';
 } else {
-    $dbDsn = 'mysql://USER:PSWD@HOST/DBNAME';
+    $dbDsn = array(
+        'phptype' => 'mysqli',
+        'username' => $_ENV['MYSQL_TEST_USER'],
+        'password' => $_ENV['MYSQL_TEST_PASSWD'],
+        'database' => $_ENV['MYSQL_TEST_DB'],
+
+        'hostspec' => empty($_ENV['MYSQL_TEST_HOST'])
+                ? null : $_ENV['MYSQL_TEST_HOST'],
+
+        'port' => empty($_ENV['MYSQL_TEST_PORT'])
+                ? null : $_ENV['MYSQL_TEST_PORT'],
+
+        'socket' => empty($_ENV['MYSQL_TEST_SOCKET'])
+                ? null : $_ENV['MYSQL_TEST_SOCKET'],
+    );
 }
